d4788a7d3d92e66db52ab76092a89060c4e4cec1
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-30 11:49:30 +0000

    adding annual sales and labels

--- bootstrap/case-study/barchart.js
+++ bootstrap/case-study/barchart.js
@@ -1,30 +1,38 @@
 var ctx = document.getElementById('annual-chart').getContext('2d');
 var annualData = [
     {
-        "2016": {
-            sales: 1000.99,
-            expences: 900.50
-        },
-        "2017": {
-            sales: 995.50,
-            expences: 900.9
-        },
-        "2018": {
-            sales: 998.9,
-            expences: 990
-        },
-        "2019": {
-            sales: 850,
-            expences: 800
-        }
+        label: 2016,
+        sales: 1000.99,
+        expences: 900.50
+    },
+    {
+        label: 2017,
+        sales: 995.50,
+        expences: 900.9
+    },
+    {
+        label: 2018,
+        sales: 998.9,
+        expences: 990
+    },
+    {
+        label: 2019,
+        sales: 850,
+        expences: 800
     }
 ];
 
+annualData.map()
+
 var data = {
-    datasets: [],
+    datasets: [{
+            label: 'Sales',
+            data: annualData.map(d => d.sales)
+        },
+    ],
 
     // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: []
+    labels: annualData.map(d => d.label)
 };
 
 new Chart(ctx, {
